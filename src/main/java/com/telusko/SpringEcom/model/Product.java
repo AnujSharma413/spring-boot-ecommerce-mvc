a3f7132fa8288b3016ca4b6f4e1da762c1c9b25a
Anuj Sharma <anujsharmajan06@gmail.com>
2026-03-28 12:16:39 +0530

    feat: add @JsonIgnore to imageData and optimize product response

--- src/main/java/com/telusko/SpringEcom/model/Product.java
+++ src/main/java/com/telusko/SpringEcom/model/Product.java
@@ -1,6 +1,7 @@
 package com.telusko.SpringEcom.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,12 +24,14 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
     private String imageName;
     private String imageType;
     @Lob
+    @JsonIgnore
     private byte[] imageData;
 
     public Product(int id) {
